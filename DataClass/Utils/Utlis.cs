4fe4f71b838239492afa0168b48c53c1979c7c42
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-07-17 16:26:43 +0300

    Utils methods

--- DataClass/Utils/Utlis.cs
+++ DataClass/Utils/Utlis.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Drawing;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace SupermarketTuto.Utils
 {
     public static class Utlis
     {
+
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
         private const int MAX_FILE_SIZE = 4194304; // 4 MBytes
         public static string HashCode(string pass)
         {
@@ -73,5 +86,522 @@ namespace SupermarketTuto.Utils
         {
 
         }
+
+        public static string ToCamelCase(this string input)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+        }
+
+        public static string SplitCamelCase(this string input)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(input, "([A-Z])", " $1", System.Text.RegularExpressions.RegexOptions.Compiled).Trim();
+        }
+
+        public static string TrimStart(this string value, string toTrim)
+        {
+            if (value.StartsWith(toTrim))
+            {
+                int startIndex = toTrim.Length;
+                return value.Substring(startIndex);
+            }
+            return value;
+        }
+
+        public static string TrimEnd(this string value, string toTrim)
+        {
+            if (value.EndsWith(toTrim))
+            {
+                int startIndex = toTrim.Length;
+                return value.Substring(0, value.Length - startIndex);
+            }
+            return value;
+        }
+
+        public static string GetString(object val, bool forSql = false, string defaultValue = "")
+        {
+            string result = defaultValue;
+
+            if (val != null)
+            {
+                result = forSql ? val.ToString().Replace("'", "''").Trim() : val.ToString().Trim();
+            }
+
+            return result;
+        }
+
+        public static string GetStringMaxChars(object val, int maxCharCount, bool forSql = false)
+        {
+            string value = GetString(val, false); // First get string
+
+            value = value.Substring(0, Math.Min(value.Length, maxCharCount));// Then cut characters if needed
+
+            // If for SQL, replace quotes
+            return forSql ? GetString(value, true) : value;
+        }
+
+        public static int GetInt(object val, int defval = -1)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return defval;
+
+            try
+            {
+                return Convert.ToInt32(val);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+
+        public static int? GetNullInt(object val, int? defval = null)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return (int?)Convert.ToInt32(val);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static Int64 GetLong(object val, long defval = -1)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return defval;
+
+            try
+            {
+                return Convert.ToInt64(val);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public static long? GetNullLong(object val, long? defval = null)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return (long?)Convert.ToInt64(val);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static DateTime? GetDate(object val, string format)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return DateTime.ParseExact(GetString(val), format, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static DateTime? GetDate(object val)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return DateTime.Parse(GetString(val));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static DateTime GetDate(object val, DateTime defaultValue)
+        {
+            try
+            {
+                return DateTime.Parse(GetString(val));
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public static float GetFloat(object val, int defval = -1)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return defval;
+
+            try
+            {
+                return float.Parse(val.ToString().Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static float? GetNullFloat(object val, float? defval = null)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return (float?)float.Parse(GetString(val).Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static double GetDouble(object val, int defval = -1)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return defval;
+
+            try
+            {
+                return double.Parse(val.ToString().Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static double? GetNullDouble(object val, double? defval = null)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return (double?)double.Parse(GetString(val).Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static decimal GetDecimal(object val, decimal defval = -1)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return defval;
+
+            try
+            {
+                decimal value = decimal.Parse(val.ToString().Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture);
+                return value;
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static decimal? GetNullDecimal(object val, decimal? defval = null)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            try
+            {
+                return decimal.Parse(GetString(val).Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return defval;
+            }
+        }
+
+        public static bool GetBool(object val, bool defaultValue = false)
+        {
+            bool result = defaultValue;
+            try
+            {
+                if (GetString(val).Equals("1") || GetString(val).ToLower().Equals("true"))
+                    result = true;
+                else if (val == null || GetString(val).Equals("0") || GetString(val).ToLower().Equals("false"))
+                    result = false;
+                else
+                    result = bool.Parse(val.ToString());
+            }
+            catch { }
+
+            return result;
+        }
+
+        public static bool? GetNullBool(object val, bool? defaultValue = null)
+        {
+            if (string.IsNullOrWhiteSpace(GetString(val)))
+                return null;
+
+            bool? result = defaultValue;
+            try
+            {
+                if (GetString(val).Equals("1") || GetString(val).ToLower().Equals("true"))
+                    result = (bool?)true;
+                else if (GetString(val).Equals("0") || GetString(val).ToLower().Equals("false"))
+                    result = (bool?)false;
+                else
+                    result = (bool?)bool.Parse(val.ToString());
+            }
+            catch { }
+
+            return result;
+        }
+
+        public static long GetUnixTimestamp(DateTime date)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan diff = date - origin;
+            return (long)Math.Floor(diff.TotalSeconds);
+        }
+
+        public static bool StringsMatchApproximately(string string1, string string2)
+        {
+            // If any is null or whitespace
+            if (string.IsNullOrWhiteSpace(string1) || string.IsNullOrWhiteSpace(string2))
+                return false;
+
+            // In case they are equal
+            if (string1.Trim().ToLower().Equals(string2.Trim().ToLower()))
+                return true;
+
+            // Get rid of commas and double spaces, then split on space
+            string[] arrName1 = string1.ToLower().Trim().Replace(",", " ").Replace("-", " ").Replace("  ", " ").Split(' ');
+            string[] arrName2 = string2.ToLower().Trim().Replace(",", " ").Replace("-", " ").Replace("  ", " ").Split(' ');
+
+            // If their length does not match
+            if (arrName1.Length != arrName2.Length)
+                return false;
+
+            // Search for all parts of name1 in name2
+            foreach (var n1 in arrName1)
+                if (arrName2.Contains(n1) == false)
+                    return false;
+
+            // Search for all parts of name2 in name1
+            foreach (var n2 in arrName2)
+                if (arrName1.Contains(n2) == false)
+                    return false;
+
+            return true;
+        }
+
+        public static DateTime GetDateFromUnixTimestamp(long unixTime, bool throwException = true, bool convertToLocal = true)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            switch (unixTime.ToString().Length)
+            {
+                case 9:
+                case 10:
+                    return convertToLocal ? epoch.AddSeconds(unixTime).ToLocalTime() : epoch.AddSeconds(unixTime);
+                case 13:
+                    return convertToLocal ? epoch.AddMilliseconds(unixTime).ToLocalTime() : epoch.AddMilliseconds(unixTime);
+            }
+
+            if (throwException)
+                throw new Exception("Error unix timestamp format for:" + unixTime.ToString());
+
+            return epoch;
+        }
+
+
+        public static string GetMD5Hash(string input)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bs = System.Text.Encoding.UTF8.GetBytes(input);
+            bs = x.ComputeHash(bs);
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+
+        public static DateTime ChangeTime(this DateTime dateTime, int hours, int minutes, int seconds, int milliseconds = 0)
+        {
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                hours,
+                minutes,
+                seconds,
+                milliseconds,
+                dateTime.Kind);
+        }
+
+        public static string GetStringWithTrailingSpaces(string Str, int TotalCharacters)
+        {
+            string result = Str;
+            for (int i = Str.Length; i < TotalCharacters; i++)
+                result += " ";
+
+            return result;
+        }
+
+        public static void CopyImageFromUrlToClipboard(string url)
+        {
+            try
+            {
+                var img = GetImageFromUrl(url);
+                /**
+                // Write the image to a temporary location (todo: purge it later)
+                var tmpFilePath = Path.Combine(Path.GetTempPath(), Path.GetFileName(url));
+                img.Save(tmpFilePath);
+
+                // Group image(s)
+                var imgCollection = new System.Collections.Specialized.StringCollection();
+                imgCollection.Add(tmpFilePath);
+
+                // Changing the drop affect to 'move' from the temp location
+                byte[] moveEffect = new byte[] { 2, 0, 0, 0 };
+                MemoryStream dropEffect = new MemoryStream();
+                dropEffect.Write(moveEffect, 0, moveEffect.Length);
+
+                // Set up our clipboard data
+                DataObject data = new DataObject();
+                data.SetFileDropList(imgCollection);
+                data.SetData("Preferred DropEffect", dropEffect);
+
+                // Push it all to the clipboard
+                Clipboard.Clear();
+                Clipboard.SetDataObject(data, true);
+                */
+                Clipboard.Clear();
+                Clipboard.SetImage(img);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("If the following error message does not make sense, please contact system administrator" + Environment.NewLine + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void OpenFileFromUrl(string url)
+        {
+            try
+            {
+                var img = GetImageFromUrl(url);
+                var tmpFilePath = Path.Combine(Path.GetTempPath(), Path.GetFileName(url));
+                img.Save(tmpFilePath);
+                System.Diagnostics.Process.Start(tmpFilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("If the following error message does not make sense, please contact system administrator" + Environment.NewLine + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static Image GetImageFromUrl(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            using (HttpWebResponse httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                using (Stream stream = httpWebReponse.GetResponseStream())
+                {
+                    return Image.FromStream(stream);
+                }
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+            }
+            return "0.0.0.0";
+        }
+
+        public static string Serialize<T>(this T obj)
+        {
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                OmitXmlDeclaration = false,
+                Encoding = Encoding.UTF8
+            };
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var serializer = new XmlSerializer(obj.GetType());
+
+            using (var stringWriter = new StringWriterUtf8())
+            {
+                using (var xmlWriter = XmlWriter.Create(stringWriter, settings))
+                {
+                    serializer.Serialize(xmlWriter, obj, namespaces);
+                }
+                return stringWriter.ToString();
+            }
+        }
+
+        public class StringWriterUtf8 : StringWriter
+        {
+            public override Encoding Encoding
+            {
+                get
+                {
+                    return Encoding.UTF8;
+                }
+            }
+        }
+
+        public static DateTime UnixTimeStampToDateTime(long unixTimeStampInMSecs)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddMilliseconds(unixTimeStampInMSecs).ToLocalTime();
+            return dtDateTime;
+        }
+
+        public static DateTime? convertToUniversalTime(DateTime? dtTime)
+        {
+            return dtTime.HasValue ? (DateTime?)dtTime.Value.ToUniversalTime() : null;
+        }
+
+        public static bool IsConnectedToInternet()
+        {
+            bool success = false;
+
+            try
+            {
+                success = InternetGetConnectedState(out int description, 0);
+            }
+            catch
+            {
+                success = false;
+            }
+
+            return success;
+        }
+
+
     }
 }
